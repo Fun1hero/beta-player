7f9611d71e644ba5ad7036e582c66dd12100f8d1
dkab <dmitry.kabanov.a@gmail.com>
2021-02-21 15:20:57 -0800

    Boilerplate, 01 to 03 functions

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import "strings"
+
+// Player struct
+type Player struct {
+	no       int
+	terrains []string
+}
+
+//Game struct
+type Game struct {
+	totalPlayers  int
+	activePlayers int
+	leftTokens    []string
+}
+
+var p Player
+var g Game
+
+var toPN string = "toP"     // "toP + player number" when we (I) understand how we get that
+var fromPN string = "fromP" // same here "fromP + player number"
+
+type fn func(string)
+
+func selectedFunction(f fn, val string) { // selectedFunction provides functionality to call specific function by its id [:2] of args string
+	f(val)
+}
+
+var functions = map[string]fn{
+	"01": g.playerNO,
+	"02": p.readMyTerrain,
+	"03": g.leftoverTokens,
+	// "04": toBeImplemented,
+	// "05": toBeImplemented,
+	// "06": toBeImplemented,
+	// "07": toBeImplemented,
+	// "08": toBeImplemented,
+	// "09": toBeImplemented,
+	// "10": toBeImplemented,
+	// "11": toBeImplemented,
+}
+
+func (gm *Game) playerNO(args string) {
+	gm.totalPlayers = int(args[len(args)-1])
+	gm.activePlayers = gm.totalPlayers
+}
+
+func (pl *Player) readMyTerrain(args string) {
+	pl.terrains = strings.Split(args[3:], ",")
+}
+
+func (gm *Game) leftoverTokens(args string) {
+	gm.leftTokens = strings.Split(args[3:], ",")
+}
+
+func main() {
+
+}
