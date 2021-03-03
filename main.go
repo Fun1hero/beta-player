428eab52be13eb9a44daec83d86e3379dbb49a7e
dkab <dmitry.kabanov.a@gmail.com>
2021-03-02 18:53:36 -0800

    fixed alignment

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func selectedFunction(f fn, val string) { // selectedFunction provides functiona
 	f(val)
 }
 
-var functions = map[string] fn {
+var functions = map[string]fn{
 	"01": g.playerNO,
 	"02": p.readMyTerrain,
 	"03": g.leftoverTokens,
@@ -46,23 +46,23 @@ var functions = map[string] fn {
 	"11": remainingWinner,
 }
 
-func (gm *Game) playerNO(args string) string{
+func (gm *Game) playerNO(args string) string {
 	gm.totalPlayers, _ = strconv.Atoi(args[len(args)-1:])
 	gm.activePlayers = gm.totalPlayers
-	return "";
+	return ""
 }
 
-func (pl *Player) readMyTerrain(args string) string{
+func (pl *Player) readMyTerrain(args string) string {
 	pl.terrains = strings.Split(args[3:], ",")
-	return ""; 
+	return ""
 }
 
-func (gm *Game) leftoverTokens(args string) string{
+func (gm *Game) leftoverTokens(args string) string {
 	gm.leftTokens = strings.Split(args[3:], ",")
-	return "";
+	return ""
 }
 
-func tokenInfoSwap(args string) string{
+func tokenInfoSwap(args string) string {
 	message := strings.Split(args[3:], ",")
 
 	if string(message[0][1]) == strconv.Itoa(p.no) {
@@ -73,89 +73,87 @@ func tokenInfoSwap(args string) string{
 	return ""
 }
 
-func remainingWinner(args string) string{
+func remainingWinner(args string) string {
 	message := strings.Split(args[3:], ",")
 
 	fmt.Printf("%s wins as the only remaining player. All others have guessed incorrectly and been disqualified. The treasures are located at %s and %s\n",
 		message[0], message[1], message[2])
-		return ""
-}
-
-func playerTurn(args string) string{
-	stringSlice := strings.Split(args, ":")
-		stringSlice2 := strings.Split(stringSlice[1], ",")
-
-		fmt.Println("Choose any two dice options from the following or choose A")
-			for j :=1;j<len(stringSlice2);j++{
-             fmt.Println(stringSlice2[j])
-			}
 	return ""
 }
 
-func chooseDice(args string) string{
+func playerTurn(args string) string {
+	stringSlice := strings.Split(args, ":")
+	stringSlice2 := strings.Split(stringSlice[1], ",")
 
+	fmt.Println("Choose any two dice options from the following or choose A")
+	for j := 1; j < len(stringSlice2); j++ {
+		fmt.Println(stringSlice2[j])
+	}
+	return ""
+}
 
-		var Dice1 string
+func chooseDice(args string) string {
+
+	var Dice1 string
 	fmt.Println("Choose first dice option")
 	fmt.Scanf("%s", &Dice1)
 
-		var Dice2 string	
-         fmt.Println("Choose second dice option")
-		fmt.Scanf("%s", &Dice2)
+	var Dice2 string
+	fmt.Println("Choose second dice option")
+	fmt.Scanf("%s", &Dice2)
 
-		var Terrain string	
-		var Player string	
+	var Terrain string
+	var Player string
 
-		fmt.Println("Choose Terrian")
-		fmt.Scanf("%s", &Terrain)
-		fmt.Println("Choose Player that you want to interrogate")
-		fmt.Scanf("%s", &Player)
+	fmt.Println("Choose Terrian")
+	fmt.Scanf("%s", &Terrain)
+	fmt.Println("Choose Player that you want to interrogate")
+	fmt.Scanf("%s", &Player)
 
-		var temp string = "05:"+Dice1+","+Dice2+","+Terrain+",P"+Player
-		return temp
+	var temp string = "05:" + Dice1 + "," + Dice2 + "," + Terrain + ",P" + Player
+	return temp
 }
-	
-	
-func SendInterrogation(args string) string{
-	
+
+func SendInterrogation(args string) string {
+
 	stringSlice := strings.Split(args, ":")
-		stringSlice2 := strings.Split(stringSlice[1], ",")
+	stringSlice2 := strings.Split(stringSlice[1], ",")
 
-		fmt.Printf("Player %s asks %s how many locations they've searched between %s and %s in %s terrain.\n",
-		stringSlice2[5],stringSlice2[4],stringSlice2[0],stringSlice2[1],stringSlice2[2])
-		
-		fmt.Printf("Player %s responds %s.\n",
-		stringSlice2[4],stringSlice2[3])
-		return ""
+	fmt.Printf("Player %s asks %s how many locations they've searched between %s and %s in %s terrain.\n",
+		stringSlice2[5], stringSlice2[4], stringSlice2[0], stringSlice2[1], stringSlice2[2])
+
+	fmt.Printf("Player %s responds %s.\n",
+		stringSlice2[4], stringSlice2[3])
+	return ""
 }
 
-func guessTokens(playerNumber string) string{
-fmt.Println("fromPn-"+fromPN)
-		var first_token string
-		var second_token string
-		fmt.Println("Choose first token: ")
-		fmt.Scanf("%s", &first_token)
-		fmt.Println("Choose second token: ")
-		fmt.Scanf("%s", &second_token)
-		var temp string = "07:P"+playerNumber+","+first_token+","+second_token
-		return temp
+func guessTokens(playerNumber string) string {
+	fmt.Println("fromPn-" + fromPN)
+	var first_token string
+	var second_token string
+	fmt.Println("Choose first token: ")
+	fmt.Scanf("%s", &first_token)
+	fmt.Println("Choose second token: ")
+	fmt.Scanf("%s", &second_token)
+	var temp string = "07:P" + playerNumber + "," + first_token + "," + second_token
+	return temp
 }
 
-func guessCorrect(args string) string{
+func guessCorrect(args string) string {
 	stringSlice := strings.Split(args, ":")
-		stringSlice2 := strings.Split(stringSlice[1], ",")
-		fmt.Printf("Player %s is correct! They have won the game.\n",
+	stringSlice2 := strings.Split(stringSlice[1], ",")
+	fmt.Printf("Player %s is correct! They have won the game.\n",
 		stringSlice2[0])
-		fmt.Printf("The treasures were located at %s and %s.\n",
-		stringSlice2[1],stringSlice2[2])
+	fmt.Printf("The treasures were located at %s and %s.\n",
+		stringSlice2[1], stringSlice2[2])
 	return ""
 }
 
 func guessIncorrect(args string) string {
 	message := strings.Split(args, ":")
 	fmt.Printf("Player %s is submitting a guess at the treasure locations!. Player %s was wrong. They are now disqualified from winning.\n",
-		message[1],message[1])
-		return ""
+		message[1], message[1])
+	return ""
 }
 
 // Reads from "toPN" named pipe
